5ed5c287b925500b68694bd9a25f5f2f6da3d2aa
kyo88765 <kyo88765@gmail.com>
2018-09-30 11:51:50 +0900

    add CameraController.cs

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private Vector3 goalPosition;
+
+    private Quaternion startRotation;
+    private Quaternion goalRotation;
+
+    public Quaternion Rotation
+    {
+        get
+        {
+            return transform.rotation;
+        }
+    }
+
+    public Quaternion HorizontalRotation
+    {
+        get
+        {
+            //TODO convert rotation to horizontal rotaion
+            return transform.rotation;
+        }
+    }
+
+    void LateUpdate()
+    {
+    }
+
+    private void UpdatePosition()
+    {
+    }
+
+    private void UpdateRotation()
+    {
+    }
+}
